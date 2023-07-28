e8be24cd408f844934a7acfdeb3f24a58be13fcc
Pavel <pavelolofinskiy@gmail.com>
2023-07-28 12:10:31 +0300

    made scraper

--- scraper.py
+++ scraper.py
@@ -3,15 +3,24 @@ import requests
 
 html_text = requests.get('https://www.castelli-cycling.com/FR/fr/Homme/Cyclisme/Top/Maillots-de-cyclisme-pour-hommes/c/Man-Cyc-Top-Jer').text
 soup = BeautifulSoup(html_text, 'lxml')
-products = soup.find_all('div', class_ = 'col-sm-6 col-md-4 mb-3 product-item')
+products = soup.find_all('div', class_='col-sm-6 col-md-4 mb-3 product-item')
+
 for product in products:
-    price = product.find('span', class_ = 'f500 price-span').text
-    description = product.find('p', class_ = 'm-0 text-center').text
+    title = product.select_one('p.m-0.h5.text-center.slideTitle').text
+    price = product.select_one('span.f500.price-span').text
+    description = product.select_one('p.m-0.text-center').text.strip()
     img_link = product.find('img')['src']
+
+    product_link = product.find('a')['href']
+    product_page = requests.get(product_link).text
+    product_soup = BeautifulSoup(product_page, 'lxml')
+    weight = product_soup.select_one('span.char-value').text
+    
     print(f'''
-    Product Price: {price}
-    Product Description: {description} 
+    Title: {title}
+    Price: {price}
+    Weight: {weight}
+    Description: {description}
     Image Link: {img_link}
     ''')
-
 
